abd2bd23bfe8a708498c8dad66d6dd69b51b62bc
Jakhongirbek <jakhonyokubjonov@gmail.com>
2025-09-25 00:49:42 +0900

    Home Screen Ui

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,26 @@
+import SearchBar from '@/components/shared/search-bar'
+import { icons } from '@/constants/icons'
+import { images } from '@/constants/images'
+import { useRouter } from 'expo-router'
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Image, ScrollView, View } from 'react-native'
 
 const Index = () => {
+	const router = useRouter()
 	return (
-		<View className='flex-1 items-center justify-center'>
-			<Text className='text-2xl font-bold text-blue-500'>
-				Welcome to the Movie App
-			</Text>
+		<View className='flex-1 bg-primary'>
+			<Image source={images.bg} className='w-full absolute z-0' />
+			<ScrollView
+				className='flex-1 px-5'
+				showsVerticalScrollIndicator={false}
+				contentContainerStyle={{ minHeight: '100%', paddingBottom: 10 }}
+			>
+				<Image source={icons.logo} className='mt-20 mb-5 mx-auto w-12' />
+				<SearchBar
+					placeholder='Search for a movie'
+					onPress={() => router.push('/search')}
+				/>
+			</ScrollView>
 		</View>
 	)
 }
