8d90de4af93a7ec05983929f45cbdf34437f4d6f
Jakhongirbek <jakhonyokubjonov@gmail.com>
2025-09-25 02:33:39 +0900

    Custom Hook

--- services/api.ts
+++ services/api.ts
@@ -7,7 +7,6 @@ export const TMBD_CONFIG = {
 	},
 }
 export const fetchMovies = async ({ query }: { query: string }) => {
-	const encodedQuery = encodeURIComponent(query)
 	const endpoint = query
 		? `discover/movie?query=${encodeURIComponent(query)}`
 		: `discover/movie?sort_by=popularity.desc`
@@ -22,19 +21,3 @@ export const fetchMovies = async ({ query }: { query: string }) => {
 	const data = await response.json()
 	return data.results
 }
-
-// const url =
-// 	'https://api.themoviedb.org/3/keyword/keyword_id/movies?include_adult=false&language=en-US&page=1'
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		accept: 'application/json',
-// 		Authorization:
-// 			'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NmZlMTQ0MjRmMWZlNjQwY2YzZDNhZWJhYTRiOTk4OCIsIm5iZiI6MTcwOTg2Mjg3Ny4yMTYsInN1YiI6IjY1ZWE2ZmRkNjY3NTFkMDE4NmFjNzliMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0o3juCQPdqVaZpyEDpTmQMXVYy4ZHny5sYEFn210B6o',
-// 	},
-// }
-
-// fetch(url, options)
-// 	.then(res => res.json())
-// 	.then(json => console.log(json))
-// 	.catch(err => console.error(err))
---
+++ services/useFetch.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react'
+
+const useFetch = <T>(fetchFunction: () => Promise<T>, autofetch = false) => {
+	const [data, setData] = useState<T | null>(null)
+	const [error, setError] = useState<Error | null>(null)
+	const [loading, setLoading] = useState<boolean>(false)
+
+	const fetchData = async () => {
+		try {
+			setLoading(true)
+			setError(null)
+			const result = await fetchFunction()
+			setData(result)
+		} catch (err) {
+			setError(
+				err instanceof Error ? err : new Error('An unknown error occurred')
+			)
+		} finally {
+			setLoading(false)
+		}
+	}
+	const reset = () => {
+		setData(null)
+		setError(null)
+		setLoading(false)
+	}
+	useEffect(() => {
+		if (autofetch) {
+			fetchData()
+		}
+	}, [])
+	return { data, error, loading, fetchData, reset }
+}
+export default useFetch
